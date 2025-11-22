83c7e2f0106e68f1ff0b5106748eb374c4bf691f
Константин Даль <djkostjan@MacBook-Pro-Konstantin.local>
2025-11-22 14:14:13 +0300

    Init commit

---
+++ cmake/FetchDependencies.cmake
@@ -0,0 +1,32 @@
+include(FetchContent)
+
+
+FetchContent_Declare(
+  spdlog
+  GIT_REPOSITORY https://github.com/gabime/spdlog.git
+  GIT_TAG v1.16.0
+)
+
+FetchContent_Declare(
+  glm
+  GIT_REPOSITORY https://github.com/g-truc/glm.git
+  GIT_TAG 1.0.2
+)
+
+FetchContent_Declare(
+  glfw
+  GIT_REPOSITORY https://github.com/glfw/glfw.git
+  GIT_TAG 3.4
+)
+
+FetchContent_Declare(
+    gtest
+    URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.zip
+)
+
+FetchContent_MakeAvailable(
+  spdlog
+  glm
+  glfw
+  gtest
+)
---
+++ test/tests.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+TEST(SimpleTest, BasicAssertions) {
+  EXPECT_EQ(2 + 2, 4);
+  EXPECT_NE(2 + 2, 5);
+}
+
+
